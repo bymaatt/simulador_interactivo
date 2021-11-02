b3f07d976be6e6cc5c7ff637874efe390112a7b9
Matias Maciel <mattias.maciel@gmail.com>
2021-11-02 00:09:13 -0300

    agregado de productos e img en carpeta

---
+++ js/productos.js
@@ -0,0 +1,14 @@
+const productos = [
+    {id: 0, nombre:'Iron Man', precio: 4500.00},
+    {id:1, nombre:'Thor', precio: 4200.00},
+    {id:2, nombre: 'Wolverine', precio: 5000.00},
+    {id:3, nombre: 'Spiderman', precio: 5500.00},
+    {id:4, nombre: 'Hulk', precio: 3500.00},
+    {id:5, nombre: 'Capitan America', precio: 4500.00},
+]
+
+const mostrarProductos = () => {
+    for ( const producto of productos){
+        
+    }
+}
